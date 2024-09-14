203bf96127f9c968cf7bdfd2ce653d4e094ba313
vynious <shawnthiah@gmail.com>
2024-09-15 01:43:35 +0800

    update clan game score entity

--- backend/elo-ranking-service/src/main/java/com/example/elorankingservice/entity/ClanGameScore.java
+++ backend/elo-ranking-service/src/main/java/com/example/elorankingservice/entity/ClanGameScore.java
@@ -8,18 +8,40 @@ import java.util.List;
 public class ClanGameScore extends GameScore {
     private Long clanId;
 
-    // One ClanGameScore has many PlayerGameScores
+    // One clan game score can have multiple player game scores
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "")  // Creates the foreign key in PlayerGameScore
+    @JoinColumn(name = "clan_game_score_id")
     private List<PlayerGameScore> playerScores;
 
-    public ClanGameScore(Long gameId, Long clanId) {
+    @Column(name = "result", nullable = false)
+    private boolean result;
+
+    @Column(name = "score", nullable = false)
+    private int score;
+
+    public ClanGameScore(Long gameId, Long clanId, boolean result) {
         super(gameId);
         this.clanId = clanId;
+        this.result = result;
     }
 
     public Long getClanId() {
         return clanId;
     }
 
+    public boolean getResult() {
+        return result;
+    }
+
+    public List<PlayerGameScore> getPlayerScores() {
+        return playerScores;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
--- backend/elo-ranking-service/src/main/java/com/example/elorankingservice/entity/GameScore.java
+++ backend/elo-ranking-service/src/main/java/com/example/elorankingservice/entity/GameScore.java
@@ -1,12 +1,16 @@
 package com.example.elorankingservice.entity;
 
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public abstract class GameScore {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long gameId;
 
@@ -16,5 +20,9 @@ public abstract class GameScore {
 
     public Long getGameId() {
         return gameId;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
