2427238d097c9d5937ba59af1010ff57afa96832
Aditya Kumar Bhardwaj <adityak.b.2023@smu.edu.sg>
2024-10-12 17:21:31 +0800

    Removed unnecessary code
    Now it's just userservice

--- frontend/src/views/ClanWarView/ClanWarView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div >
-    <h1>This is an ClanWarView page</h1>
-  </div>
-</template>
-
-<style>
-
-</style>
--- frontend/src/views/ProfileView/ProfileView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1>This is an PROFILE page</h1>
-  </div>
-</template>
-
-<style>
-
-</style>
