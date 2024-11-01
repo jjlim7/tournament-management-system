e623e029b6c306c7e2283ffb3536057427e3ab4a
vynious <shawnthiah@gmail.com>
2024-11-01 23:19:21 +0800

    update dockerfile for services; add new endpoints on elo-ranking to get latest rank

--- backend/elo-ranking-service/src/main/java/com/example/elorankingservice/repository/PlayerGameScoreRepository.java
+++ backend/elo-ranking-service/src/main/java/com/example/elorankingservice/repository/PlayerGameScoreRepository.java
@@ -1,9 +1,14 @@
 package com.example.elorankingservice.repository;
 
+import com.example.elorankingservice.entity.ClanEloRank;
+import com.example.elorankingservice.entity.PlayerEloRank;
 import com.example.elorankingservice.entity.PlayerGameScore;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PlayerGameScoreRepository extends BaseGameScoreRepository<PlayerGameScore> {
